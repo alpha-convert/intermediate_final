f870745420b58349b483fc70e1ca0ed6910950bc
Joseph Cutler <joseph_cutler@watkinson.org>
2015-02-25 09:19:42 -0500

    finished compiler!

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from parser import Parser
+from ops import Ops
+import sys
 class Compiler:
     def __init__(self,file):
         parser = Parser(file)
@@ -9,9 +11,26 @@ class Compiler:
         parser.find_jumps()
         #At this point, the code has been parsed, jumps have been calculated and the code inserted.
         self.code = parser.tokens
+        self.ops = Ops()
         #print self.code
 
     def translate_to_opcodes(self):
-        
+        hexcode = []
+        for i, item in enumerate(self.code):
+            hexcode.append(self.ops.contains(item)) #If the item is one of the opcodes, return the opcode in hex. If it isnt, return the number in hex.
+        print hexcode
+        # print self.code
+        self.compiled = hexcode
+
+    def write_to_file(self):
+        file = open("file.bin","wb")
+        for op in self.compiled:
+            print op
+            file.write(op)
+        file.close()
+
+sys.dont_write_bytecode = True
 
 compiler = Compiler("testing/test.p15")
+compiler.translate_to_opcodes()
+compiler.write_to_file()
---
+++ compiler/ops.py
@@ -0,0 +1,50 @@
+class Ops:
+    def __init__(self):
+        self.ops = {
+            #REGISTERS
+            "%SP":"\xF1",
+            "%MP":"\xF2",
+            "%CL":"\xF3",
+            "%AL":"\xF4",
+            "%GP1":"\xF5",
+            "%GP2":"\xF6",
+            "%GP3":"\xF7",
+            "%GP4":"\xF8",
+            "%GP5":"\xF9",
+            "%GP6":"\xFA",
+            "%GP7":"\xFB",
+            "%GP8":"\xFC",
+            "%SY1":"\xFD",
+            "%SY2":"\xFE",
+            "%SY3":"\xFF",
+
+            #ARITHMATIC OPERATIONS
+            "NOP":"\x00",
+            "ADD":"\x01",
+            "SUB":"\x02",
+            "MUL":"\x03",
+            "DIV":"\x04",
+            "AND":"\x05",
+            "OR":"\x06",
+            "NOT":"\x07",
+            "XOR":"\x08",
+            "LSF":"\x09",
+            "RSF":"\x0A",
+
+            #REGISTER/MEMORY OPS
+            "MOV":"\x0B",
+            "PSH":"\x0C",
+            "POP":"\x0D",
+            "SYS":"\x0E",
+            "JMP":"\x0F",
+            "CMP":"\x11",
+            "HLT":"\x12"
+        }
+
+    def contains(self,str):
+        if str in self.ops:
+            #print str
+            return self.ops[str]
+        else:
+            print chr(int(str))
+            return chr(int(str))
